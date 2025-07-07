7a66d189eb6f0fa5bc691eb749e12a2141271603
majid <majid.gha3mi.p@gmail.com>
2025-07-07 11:49:44 +0330

    AppColors add

---
+++ lib/core/app_colors.dart
@@ -0,0 +1,13 @@
+import 'dart:ui';
+
+class AppColors {
+  List darkModeColors = [
+    ///backgraund
+    Color(0xff222831),
+    //secondery
+     Color(0xff393e46)];
+
+  List lightModeColors = [ 
+      Color.fromARGB(255, 255, 255, 255),
+     Color.fromARGB(255, 243, 242, 242),];
+}
