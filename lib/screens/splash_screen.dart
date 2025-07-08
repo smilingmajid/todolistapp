e24d08f97f79cd958427cf135c8bf76771faa40e
majid <majid.gha3mi.p@gmail.com>
2025-07-08 12:39:58 +0330

    splash screen style by animated text kit

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -1,13 +1,42 @@
+import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:flutter/material.dart';
 
 class SplashScreen extends StatelessWidget {
+  const SplashScreen({super.key});
+
   @override
   Widget build(BuildContext context) {
 
-    
+  
     return Scaffold(
       body: Center(
-        child: Text('Splash Screen'),
+        child: SizedBox(
+  width: 250.0,
+  child: DefaultTextStyle(
+    style: const TextStyle(
+      fontSize: 30.0,
+      color: Colors.black,
+     // fontFamily: 'Agne',
+    ),
+    child: AnimatedTextKit(
+      animatedTexts: [
+        TypewriterAnimatedText('ToDoList App',
+          speed: const Duration(milliseconds: 100),
+          cursor: '|',
+          textStyle: const TextStyle(
+            fontSize: 30.0,
+            color: Colors.black,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+       
+      ],
+      onTap: () {
+        print("Tap Event");
+      },
+    ),
+  ),
+),
       ),
     );
   }
