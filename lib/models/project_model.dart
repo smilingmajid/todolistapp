56c250cd1577f050a496e7ee7a457d33d13645ff
majid <majid.gha3mi.p@gmail.com>
2025-07-17 12:40:21 +0330

    projectId add projectmodel

--- lib/models/project_model.dart
+++ lib/models/project_model.dart
@@ -1,38 +1,27 @@
 import 'dart:ui';
-import 'task_model.dart';
 
 class ProjectModel {
+  final String id;
   final String title;
   final int colorValue;
-  final List<TaskModel> tasks;
 
   ProjectModel({
+    required this.id,
     required this.title,
     required this.colorValue,
-    required this.tasks,
   });
 
   Color get color => Color(colorValue);
 
-  
-  int get completedTasksCount => tasks.where((t) => t.isDone).length;
-
-  int get totalTasksCount => tasks.length;
-
-  
-  String get progressFraction => "$completedTasksCount/$totalTasksCount";
-
   Map<String, dynamic> toMap() => {
+        'id': id,
         'title': title,
         'colorValue': colorValue,
-        'tasks': tasks.map((e) => e.toMap()).toList(),
       };
 
   factory ProjectModel.fromMap(Map<String, dynamic> map) => ProjectModel(
+        id: map['id'],
         title: map['title'],
         colorValue: map['colorValue'],
-        tasks: List<Map<String, dynamic>>.from(map['tasks'] ?? [])
-            .map((e) => TaskModel.fromMap(e))
-            .toList(),
       );
 }
