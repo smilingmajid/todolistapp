da2d8481dae57e4b8df78edb6740cf34a0c9e9f5
majid <majid.gha3mi.p@gmail.com>
2025-07-26 13:52:25 +0330

    add taslBox to taskController

--- lib/controllers/task_controller.dart
+++ lib/controllers/task_controller.dart
@@ -1,22 +1,42 @@
 import 'package:get/get.dart';
+import 'package:hive/hive.dart';
 import '../models/task_model.dart';
 
 class TaskController extends GetxController {
   RxList<TaskModel> tasks = <TaskModel>[].obs;
+  late final Box<TaskModel> taskBox;
+
+  @override
+  void onInit() {
+    taskBox = Hive.box<TaskModel>('taskBox');
+    tasks.value = taskBox.values.toList();
+    super.onInit();
+  }
 
   void addTask(TaskModel task) {
     tasks.add(task);
+    taskBox.add(task);
   }
 
   void deleteTask(int index) {
     if (index >= 0 && index < tasks.length) {
       tasks.removeAt(index);
+      taskBox.deleteAt(index);
     }
   }
 
   void toggleDone(int index) {
     if (index >= 0 && index < tasks.length) {
       final task = tasks[index];
+         final newTask = TaskModel(
+      title: task.title,
+      description: task.description,
+      deadline: task.deadline,
+      isDone: !task.isDone,
+      createdAt: task.createdAt,
+      projectId: task.projectId,
+    );
+    /*
       tasks[index] = TaskModel(
         title: task.title,
         description: task.description,
@@ -24,7 +44,10 @@ class TaskController extends GetxController {
         isDone: !task.isDone,
         createdAt: task.createdAt,
         projectId: task.projectId,
-      );
+      );*/
+         tasks[index] = newTask;
+    taskBox.putAt(index, newTask); // آپدیت در Hive
+
     }
   }
 
