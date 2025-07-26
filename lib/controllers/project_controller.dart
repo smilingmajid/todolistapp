7cf17afd6150a8c49836ae248d9531adfa30f80b
majid <majid.gha3mi.p@gmail.com>
2025-07-26 13:25:31 +0330

    add projectbox to project controller

--- lib/controllers/project_controller.dart
+++ lib/controllers/project_controller.dart
@@ -1,24 +1,44 @@
 import 'package:get/get.dart';
+import 'package:hive/hive.dart';
 import 'package:uuid/uuid.dart';
 import '../models/project_model.dart';
 
 class ProjectController extends GetxController {
   RxList<ProjectModel> projectList = <ProjectModel>[].obs;
+  late final Box<ProjectModel> projectBox;
   final _uuid = Uuid();
+
+  @override
+  void onInit() {
+    projectBox = Hive.box<ProjectModel>('projectBox');
+    projectList.value = projectBox.values.toList();
+    super.onInit();
+  }
 
   void addProject(String title, int colorValue) {
     if (title.trim().isNotEmpty) {
-      projectList.add(ProjectModel(
-        id: _uuid.v4(), 
-        title: title.trim(),
-        colorValue: colorValue,
-      ));
+      projectList.add(
+        ProjectModel(
+          id: _uuid.v4(),
+          title: title.trim(),
+          colorValue: colorValue,
+        ),
+      );
+
+      projectBox.add(
+        ProjectModel(
+          id: _uuid.v4(),
+          title: title.trim(),
+          colorValue: colorValue,
+        ),
+      );
     }
   }
 
   void deleteProject(int index) {
     if (index >= 0 && index < projectList.length) {
       projectList.removeAt(index);
+      projectBox.deleteAt(index);
     }
   }
 }
