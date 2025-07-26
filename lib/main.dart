eb03ef8bfff8a88aed1b7baa9e0035d43085ed2f
majid <majid.gha3mi.p@gmail.com>
2025-07-26 14:37:59 +0330

    translation setting add to main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:hive_flutter/hive_flutter.dart';
 
 import '../screens/splash_screen.dart';
 import 'bindings/my_bindings.dart';
+import 'core/app_translations.dart';
 import 'models/project_model.dart';
 import 'models/task_model.dart';
 
@@ -26,6 +27,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
+       translations: AppTranslations(), 
+       locale:Locale('en', 'US'),
+       fallbackLocale: Locale('fa', 'IR'),
       title: 'todolistapp',
       debugShowCheckedModeBanner: false,
       home: SplashScreen(),
