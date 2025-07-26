5938a0607a37142d9a2c98832069b4199919a558
majid <majid.gha3mi.p@gmail.com>
2025-07-26 13:18:30 +0330

    hive init on main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
 import '../screens/splash_screen.dart';
 import 'bindings/my_bindings.dart';
+import 'models/project_model.dart';
+import 'models/task_model.dart';
 
-void main() {
+Future<void> main() async {
+   WidgetsFlutterBinding.ensureInitialized();
+
+  await Hive.initFlutter();
+
+  Hive.registerAdapter(ProjectModelAdapter());
+  Hive.registerAdapter(TaskModelAdapter());
+  await Hive.openBox<ProjectModel>('projectBox');
+  await Hive.openBox<TaskModel>('taskBox');
+
   runApp(const MyApp());
 }
 
