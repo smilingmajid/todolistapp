96fc0929a1c146084aaba5e2a328784fa8c3567f
majid <majid.gha3mi.p@gmail.com>
2025-07-26 13:15:57 +0330

    taskmodel.g and projectmodel.g created

---
+++ lib/models/project_model.g.dart
@@ -0,0 +1,47 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'project_model.dart';
+
+// **************************************************************************
+// TypeAdapterGenerator
+// **************************************************************************
+
+class ProjectModelAdapter extends TypeAdapter<ProjectModel> {
+  @override
+  final int typeId = 0;
+
+  @override
+  ProjectModel read(BinaryReader reader) {
+    final numOfFields = reader.readByte();
+    final fields = <int, dynamic>{
+      for (int i = 0; i < numOfFields; i++) reader.readByte(): reader.read(),
+    };
+    return ProjectModel(
+      id: fields[0] as String,
+      title: fields[1] as String,
+      colorValue: fields[2] as int,
+    );
+  }
+
+  @override
+  void write(BinaryWriter writer, ProjectModel obj) {
+    writer
+      ..writeByte(3)
+      ..writeByte(0)
+      ..write(obj.id)
+      ..writeByte(1)
+      ..write(obj.title)
+      ..writeByte(2)
+      ..write(obj.colorValue);
+  }
+
+  @override
+  int get hashCode => typeId.hashCode;
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is ProjectModelAdapter &&
+          runtimeType == other.runtimeType &&
+          typeId == other.typeId;
+}
