ccf33f4f338feacb390ed5435a8544ece7c47aa8
majid <majid.gha3mi.p@gmail.com>
2025-07-26 21:42:11 +0330

    empty widget of  HomeScreen translate

--- lib/core/app_translations.dart
+++ lib/core/app_translations.dart
@@ -3,27 +3,23 @@ import 'package:get/get_navigation/src/root/internacionalization.dart';
 class AppTranslations extends Translations {
   @override
   Map<String, Map<String, String>> get keys => {
-        'en_US': flatten({
-          'HomeScreen': {
-            'AppBar': 'HomeScreen',
-            'message': 'No Project',
-          },
-          'TaskScreen': {
-            'AppBar': 'Tasks',
-          },
-        }),
-        'fa_IR': flatten({
-          'HomeScreen': {
-            'AppBar': 'خانه',
-            'message': 'هیچ پروژه‌ای نیست',
-          },
-          'TaskScreen': {
-            'AppBar': 'وظایف',
-          },
-        }),
-      };
+    'en_US': flatten({
+      'HomeScreen': {
+        'AppBar': 'HomeScreen',
+        'NoProject': 'No projects yet.\nTap + to add a new project.',
+      },
+      'TaskScreen': {'AppBar': 'Tasks'},
+    }),
+    'fa_IR': flatten({
+      'HomeScreen': {
+        'AppBar': 'خانه',
+        'NoProject':
+            'هنوز هیچ پروژه‌ای وجود ندارد.\nبرای افزودن پروژه جدید، روی + بزنید.',
+      },
+      'TaskScreen': {'AppBar': 'وظایف'},
+    }),
+  };
 
- 
   Map<String, String> flatten(Map<String, dynamic> map, [String prefix = '']) {
     Map<String, String> result = {};
     map.forEach((key, value) {
--- lib/core/widgets/empty_widget.dart
+++ lib/core/widgets/empty_widget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:lottie/lottie.dart';
 
 Widget emptyWidget() {
@@ -7,7 +8,7 @@ Widget emptyWidget() {
       Lottie.asset('assets/empty.json'),
 
       Text(
-        'No projects yet.\nTap + to add a new project.',
+        'HomeScreen.NoProject'.tr,
         textAlign: TextAlign.center,
         style: TextStyle(
           fontSize: 16,
