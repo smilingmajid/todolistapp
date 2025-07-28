14f2006edb651cd3954fa444f79ecfd68011b2af
majid <majid.gha3mi.p@gmail.com>
2025-07-28 10:18:35 +0330

    LanguageController update by flags picture

--- lib/controllers/language_controller.dart
+++ lib/controllers/language_controller.dart
@@ -1,37 +1,35 @@
 import 'package:get/get.dart';
 import 'package:flutter/material.dart';
 
-
 class LanguageController extends GetxController {
+  final List<String> languageCodes = ['en', 'de', 'fa', 'ar'];
+  final List<String> flagPaths = [
+    'assets/flags/us.png',
+    'assets/flags/de.png',
+    'assets/flags/ir.png',
+    'assets/flags/sa.png',
+  ];
+
   final RxInt currentIndex = 0.obs;
 
-  final List<Locale> supportedLocales = const [
-    Locale('en', 'US'),
-    Locale('de', 'DE'),
-    Locale('fa', 'IR'),
-    Locale('ar', 'SA'),
-  ];
-
-  final List<String> languageNames = [
-    'English',
-    'Deutsch',
-    'فارسی',
-    'العربية',
-  ];
-
-  final List<String> flagCodes = [
-    'us',
-    'de',
-    'ir',
-    'sa',
-  ];
-
   void nextLanguage() {
-    currentIndex.value = (currentIndex.value + 1) % supportedLocales.length;
-    Get.updateLocale(supportedLocales[currentIndex.value]);
+    currentIndex.value = (currentIndex.value + 1) % languageCodes.length;
+    final langCode = languageCodes[currentIndex.value];
+    final locale = _getLocaleFromLangCode(langCode);
+    Get.updateLocale(locale);
   }
 
-  String get currentFlagCode => flagCodes[currentIndex.value];
-
-  String get currentLanguageName => languageNames[currentIndex.value];
+  Locale _getLocaleFromLangCode(String code) {
+    switch (code) {
+      case 'fa':
+        return const Locale('fa', 'IR');
+      case 'de':
+        return const Locale('de', 'DE');
+      case 'ar':
+        return const Locale('ar', 'SA');
+      case 'en':
+      default:
+        return const Locale('en', 'US');
+    }
+  }
 }
