20451589389231602dc726577521dcf3845fc933
majid <majid.gha3mi.p@gmail.com>
2025-07-28 11:36:18 +0330

    hive inint and call local on main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -5,12 +5,13 @@ import 'package:flutter_localizations/flutter_localizations.dart';
 
 import '../screens/splash_screen.dart';
 import 'bindings/my_bindings.dart';
+import 'controllers/language_controller.dart';
 import 'core/app_translations.dart';
 import 'models/project_model.dart';
 import 'models/task_model.dart';
 
 Future<void> main() async {
-   WidgetsFlutterBinding.ensureInitialized();
+  WidgetsFlutterBinding.ensureInitialized();
 
   await Hive.initFlutter();
 
@@ -18,35 +19,39 @@ Future<void> main() async {
   Hive.registerAdapter(TaskModelAdapter());
   await Hive.openBox<ProjectModel>('projectBox');
   await Hive.openBox<TaskModel>('taskBox');
+  await Hive.openBox('settings');
+  final box = Hive.box('settings');
+  String? langCode = box.get('langCode', defaultValue: 'en');
+  Locale locale = LanguageController.getLocaleFromLangCode(langCode!);
 
-  runApp(const MyApp());
+  runApp(MyApp(initialLocale: locale));
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  final Locale initialLocale;
+  const MyApp({super.key, required this.initialLocale});
 
   @override
   Widget build(BuildContext context) {
-  return GetMaterialApp(
-  translations: AppTranslations(),
-  locale: Locale('en', 'US'),
-  fallbackLocale: Locale('en', 'US'),
-  supportedLocales: const [
-    Locale('en', 'US'),
-    Locale('de', 'DE'),
-    Locale('fa', 'IR'),
-    Locale('ar', 'SA'),
-  ],
-  localizationsDelegates: const [
-    GlobalMaterialLocalizations.delegate,
-    GlobalWidgetsLocalizations.delegate,
-    GlobalCupertinoLocalizations.delegate,
-  ],
+    return GetMaterialApp(
+      translations: AppTranslations(),
+      locale: initialLocale,
+      fallbackLocale: Locale('en', 'US'),
+      supportedLocales: const [
+        Locale('en', 'US'),
+        Locale('de', 'DE'),
+        Locale('fa', 'IR'),
+        Locale('ar', 'SA'),
+      ],
+      localizationsDelegates: const [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
       title: 'todolistapp',
       debugShowCheckedModeBanner: false,
       home: SplashScreen(),
       initialBinding: MyBindings(),
-);
-
+    );
   }
 }
