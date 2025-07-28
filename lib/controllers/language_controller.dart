bf3ec594d54b4d5c6dba606ee78101a1490fccf8
majid <majid.gha3mi.p@gmail.com>
2025-07-28 10:56:13 +0330

    save language on hive in language controller

--- lib/controllers/language_controller.dart
+++ lib/controllers/language_controller.dart
@@ -1,5 +1,6 @@
 import 'package:get/get.dart';
 import 'package:flutter/material.dart';
+import 'package:hive/hive.dart';
 
 class LanguageController extends GetxController {
   final List<String> languageCodes = ['en', 'de', 'fa', 'ar'];
@@ -12,14 +13,27 @@ class LanguageController extends GetxController {
 
   final RxInt currentIndex = 0.obs;
 
+  @override
+  void onInit() {
+    super.onInit();
+    _loadLanguage();
+  }
+
   void nextLanguage() {
     currentIndex.value = (currentIndex.value + 1) % languageCodes.length;
     final langCode = languageCodes[currentIndex.value];
-    final locale = _getLocaleFromLangCode(langCode);
+    final locale = getLocaleFromLangCode(langCode);
+    Hive.box('settings').put('langCode', langCode);
+
     Get.updateLocale(locale);
   }
 
-  Locale _getLocaleFromLangCode(String code) {
+  void _loadLanguage() {
+    final langCode = Hive.box('settings').get('langCode', defaultValue: 'en');
+    currentIndex.value = languageCodes.indexOf(langCode);
+  }
+
+  static Locale getLocaleFromLangCode(String code) {
     switch (code) {
       case 'fa':
         return const Locale('fa', 'IR');
