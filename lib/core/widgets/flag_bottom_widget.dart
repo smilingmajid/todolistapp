e62904bf6decc8a63d92aafef42038a0eee740db
majid <majid.gha3mi.p@gmail.com>
2025-07-29 10:41:04 +0330

    flagDropdownWidget craeted

--- lib/core/widgets/flag_bottom_widget.dart
+++ lib/core/widgets/flag_bottom_widget.dart
@@ -1,45 +1,17 @@
-
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:hive/hive.dart';
-
 import '../../controllers/language_controller.dart';
-/*
-
-Widget flagBottomWidget(LanguageController languageController) {
-  return Obx(
-    () => IconButton(
-      iconSize: 40,
-      icon: Image.asset(
-        languageController.flagPaths[languageController.currentIndex.value],
-        width: 40,
-        height: 40,
-      ),
-      onPressed: languageController.nextLanguage,
-    ),
-  );
-}
-*/
-
 
 Widget flagDropdownWidget(LanguageController languageController, bool isDark) {
   return Obx(
     () => DropdownButton<int>(
       value: languageController.currentIndex.value,
-      icon: const Icon(Icons.arrow_drop_down),
+      icon: Icon(Icons.arrow_drop_down, color: isDark ? Colors.white : Colors.black),
       dropdownColor: isDark ? Colors.black : Colors.white,
-      underline: const SizedBox(), // حذف خط زیر دکمه
+      underline: SizedBox(),
       onChanged: (int? newIndex) {
         if (newIndex != null) {
-          languageController.currentIndex.value = newIndex;
-
-          final langCode = languageController.languageCodes[newIndex];
-          final locale = LanguageController.getLocaleFromLangCode(langCode);
-
-          Hive.box('settings').put('langCode', langCode);
-          languageController.isRtl.value =
-              languageController._isRtl(langCode);
-          Get.updateLocale(locale);
+          languageController.setLanguageByIndex(newIndex);
         }
       },
       items: List.generate(
@@ -53,7 +25,7 @@ Widget flagDropdownWidget(LanguageController languageController, bool isDark) {
                 width: 24,
                 height: 24,
               ),
-              const SizedBox(width: 8),
+              SizedBox(width: 8),
               Text(
                 languageController.languageCodes[index].toUpperCase(),
                 style: TextStyle(
