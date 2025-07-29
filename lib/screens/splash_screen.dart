0092a7e65848f8283fcc9ff7d572c4ba4c1e7233
majid <majid.gha3mi.p@gmail.com>
2025-07-29 13:58:36 +0330

    task.json add to Splasscreen

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -1,42 +1,14 @@
-import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
 
 class SplashScreen extends StatelessWidget {
   const SplashScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-
-  
     return Scaffold(
       body: Center(
-        child: SizedBox(
-  width: 250.0,
-  child: DefaultTextStyle(
-    style: const TextStyle(
-      fontSize: 30.0,
-      color: Colors.black,
-     // fontFamily: 'Agne',
-    ),
-    child: AnimatedTextKit(
-      animatedTexts: [
-        TypewriterAnimatedText('ToDoList App',
-          speed: const Duration(milliseconds: 100),
-          cursor: '|',
-          textStyle: const TextStyle(
-            fontSize: 30.0,
-            color: Colors.black,
-            fontWeight: FontWeight.bold,
-          ),
-        ),
-       
-      ],
-      onTap: () {
-      
-      },
-    ),
-  ),
-),
+        child: SizedBox(width: 250.0, child: Lottie.asset('assets/task.json')),
       ),
     );
   }
