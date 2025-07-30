707c3fcbc2b4afdf9f56abdf680f8805c478863f
majid <majid.gha3mi.p@gmail.com>
2025-07-30 14:55:12 +0330

    Safe Area and SplahController call on SplashScreen

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -1,14 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
 import 'package:lottie/lottie.dart';
+import 'package:todolistapp/controllers/splash_controller.dart';
 
 class SplashScreen extends StatelessWidget {
   const SplashScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: SizedBox(width: 500, child: Lottie.asset('assets/task.json')),
+     Get.find<SplashController>();
+    return SafeArea(
+      child: Scaffold(
+        body: Center(
+          child: SizedBox(width: 500, child: Lottie.asset('assets/task.json')),
+        ),
       ),
     );
   }
