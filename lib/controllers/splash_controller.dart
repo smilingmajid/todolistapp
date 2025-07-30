e632f02069928670c89f4a44fcfb603a2dcf6c59
majid <majid.gha3mi.p@gmail.com>
2025-07-30 14:57:45 +0330

    Delayed on SplahController increased to 5000 milisecend

--- lib/controllers/splash_controller.dart
+++ lib/controllers/splash_controller.dart
@@ -1,22 +1,14 @@
-
 import 'package:get/get.dart';
 
 import '../screens/home_screen.dart';
-
-
 
 class SplashController extends GetxController {
   @override
   void onInit() {
     super.onInit();
 
-    Future.delayed(
-      const Duration(milliseconds: 3500),
-      () async {
-        Get.off(
-          () =>  HomeScreen(),
-        );
-      },
-    ).then((_) {});
+    Future.delayed(const Duration(milliseconds: 5000), () async {
+      Get.off(() => HomeScreen());
+    }).then((_) {});
   }
 }
