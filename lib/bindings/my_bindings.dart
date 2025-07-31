248df2a2f3c1b2fd9b5958ca9b9ffe13b6db8b98
majid <majid.gha3mi.p@gmail.com>
2025-07-31 14:29:15 +0330

    OnboardingController lazyPut on bindings

--- lib/bindings/my_bindings.dart
+++ lib/bindings/my_bindings.dart
@@ -3,6 +3,7 @@ import 'package:todolistapp/controllers/project_controller.dart';
 import '../controllers/date_controller.dart';
 import '../controllers/adrawer_controller.dart';
 import '../controllers/language_controller.dart';
+import '../controllers/onboarding_controller.dart';
 import '../controllers/splash_controller.dart';
 import '../controllers/task_controller.dart';
 import '../controllers/theme_controller.dart';
@@ -10,14 +11,7 @@ import '../controllers/theme_controller.dart';
 class MyBindings implements Bindings {
   @override
   void dependencies() {
-    /*
-    Get.put(ThemeController(),);
-     Get.put(ProjectController(),);
-    Get.put(SplashController(),);
-    Get.put(TaskController(),);
-     Get.put(DateController(),);
-     Get.put(ADrawerController(),);
-        Get.put(LanguageController(),);*/
+
     Get.lazyPut(() => TaskController(), fenix: true);
     Get.lazyPut(() => ProjectController(), fenix: true);
     Get.lazyPut(() => DateController(), fenix: true);
@@ -25,5 +19,6 @@ class MyBindings implements Bindings {
     Get.lazyPut(() => LanguageController(), fenix: true);
     Get.lazyPut(() => ThemeController(), fenix: true);
     Get.lazyPut(() => SplashController(), fenix: true);
+    Get.lazyPut(() => OnboardingController(),fenix: true);
   }
 }
